d0e7461007ec19bbede271eccfa3364dd06cb9e8
07.Mayank-raj <2003.mayank.raj@gmail.com>
2022-09-13 21:13:44 +0530

    wrong code files added!

---
+++ isPalindrome.js
@@ -0,0 +1,7 @@
+function isPalindrome(str){
+    for (i of str){
+        console.log(i)
+    }
+}
+
+isPalindrome('jasfljfla');
---
+++ primeProblem.js
@@ -0,0 +1,6 @@
+function primeNumber(x){
+    for(let i = 3; i<=x;i++){
+        console.log(i);
+    }
+}
+primeNumber(34)
