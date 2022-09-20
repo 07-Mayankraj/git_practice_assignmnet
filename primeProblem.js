cae772b33fe093b09664bc9634d6ead6e020135f
Bhupender Soni <official.soni532@gmail.com>
2022-09-20 18:57:50 +0530

    asdkfja;s

--- primeProblem.js
+++ primeProblem.js
@@ -1,12 +1,11 @@
 function primeNumber(x) {
+    // 
     let flag = true;
-    for (let i = 2; i <= x; i++) {
-        for (let j = 2; j < i; j++) {
-            if (i % j == 0) {
+    for (let i = 2; i <= x; i++) { 
+         if (i % j == 0) {
                 flag = false;
                 break;
-            }
-        }
+            }  
     }
     console.log(flag ? "Yes" : "No")
 }
